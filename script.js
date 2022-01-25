d7aafff34e42f2b263d8c24bba41b6e257c65c68
Girish Soni <71958409+GirishSoni-lorka@users.noreply.github.com>
2022-01-25 07:43:37 +0530

    change in js file

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // For image 1
 var randomNumber1 = Math.floor((Math.random() * 6) + 1);
-var randomDiceImage1 = "images/Dice" + randomNumber1 + ".png";
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
 document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage1);
 
 // For image 2
 var randomNumber2 = Math.floor((Math.random() * 6) + 1);
-document.querySelectorAll("img")[1].setAttribute("src", "images/Dice" + randomNumber2 + ".png");// in 1 line
+document.querySelectorAll("img")[1].setAttribute("src", "images/dice" + randomNumber2 + ".png");// in 1 line
 
 // for wining logic
 if(randomNumber1 > randomNumber2){
